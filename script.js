f7aec76f0bec26a3e72ff3fa1d52306510535a8b
vitor schneiker <vitor.alvaresB@gmail.com>
2024-06-16 20:45:05 +0000

    Criação do tema-helper.js responsavel por mudar o tema

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
-let tema = "claro"
+import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
-const botaotema = document.querySelector(".tema button")
-botaotema.addEventListener("click", trocarTema)
-
+const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
 
-function trocarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-    
-    
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-    
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
 
-}
-
-function verificarTema()  {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-    }
-
-}
-
-verificarTema()
+verificarTema(body, botaoTema)
