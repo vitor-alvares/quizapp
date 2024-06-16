ec5e4f2d91d64aa556d0b42131832fac594c902a
vitor schneiker <vitor.alvaresB@gmail.com>
2024-06-16 07:47:42 +0000

    Primeiro ajuste do tema no javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+let tema = "claro"
+
+const botaotema = document.querySelector(".tema button")
+botaotema.addEventListener("click", trocarTema)
+
+const body = document.querySelector("body")
+
+
+function trocarTema() {
+    if (localStorage.getItem("tema")) {
+        tema = localStorage.getItem("tema")
+    }
+    
+    
+    if (tema === "claro") {
+        body.classList.add("escuro")
+        localStorage.setItem("tema", "escuro")
+    
+    } else {
+        body.classList.remove("escuro")
+        localStorage.setItem("tema", "claro")
+    }
+
+}
+
+function verificarTema()  {
+    if (localStorage.getItem("tema")) {
+        tema = localStorage.getItem("tema")
+    }
+
+    if (tema === "escuro") {
+        body.classList.add("escuro")
+    }
+
+}
+
+verificarTema()
