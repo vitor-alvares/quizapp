87428e0fddd7a500bc48da878587dbccc13bf5d7
vitor schneiker <vitor.alvaresB@gmail.com>
2024-06-16 20:51:12 +0000

    Criação e adição do JS para trocar o tema

---
+++ pages/Quiz/quiz.js
@@ -0,0 +1,11 @@
+import { verificarTema } from "../../helpers/tema-helper.js";
+
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
+
+
+verificarTema(body, botaoTema)
