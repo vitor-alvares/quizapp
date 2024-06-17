f75fa0dc7825f1118a808e27e4881c3b18dcb8b1
vitor schneiker <vitor.alvaresB@gmail.com>
2024-06-17 00:19:15 +0000

    Alteração dinamica do assunto da página

---
+++ pages/quiz/quiz.js
@@ -0,0 +1,27 @@
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
+
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
+
+verificarTema(body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `icone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+
+}
+
+alterarAssunto()
--- script.js
+++ script.js
@@ -9,3 +9,14 @@ botaoTema.addEventListener("click", () => {
 
 
 verificarTema(body, botaoTema)
+
+const botoesAssuntos = document.querySelectorAll(".assuntos button")
+botoesAssuntos.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
